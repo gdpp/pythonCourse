a39945822aa8161aec88aca5bf5e360fcd899ce8
gdpp <gustavo.perez.231191@gmail.com>
2022-06-14 12:51:03 -0500

    Delete 1.basics directory
    duplicate folder

--- 1.basics/the-count-method.py
+++
@@ -1,9 +0,0 @@
-name = "queueing"
-
-print(name.count("e"))
-print(name.count("u"))
-print(name.count("q"))
-print(name.count("Z"))
-print(name.count("ue"))
-
-print(name.count("e") + name.count("u"))
